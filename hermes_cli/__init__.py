78438a4c249169e8831abefe2d5893ca85e5f3d6
huzihao <huzihao@minieye.cc>
2026-05-10 14:23:45 +0800

    release(hermes-agent): v0.13.7 overlay

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.13.6"
-__release_date__ = "2026.5.8"
+__version__ = "0.13.7"
+__release_date__ = "2026.5.10"
 
 
 def _ensure_utf8():
